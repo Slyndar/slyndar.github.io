675b51f5672f1e71493902c581d05c3e02b02d56
Philipp Wulff <philipp-wulff@gmx.net>
2023-11-30 19:10:21 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+function calculate(){
+    let num1 = Number(document.getElementById('num1').value);
+    let num2 = Number(document.getElementById('num2').value);
+    var res = num1 + num2;
+    document.getElementById('loesung').innerHTML = res;
+}
+
+
